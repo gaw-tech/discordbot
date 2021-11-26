b97f56d6217dd7121ff275628fb25e3ec6d9399b
gaw-tech <74785284+gaw-tech@users.noreply.github.com>
2021-11-26 03:56:43 +0100

    Music Bot is finally working on virtual machine :)

--- GloriousKingJulien/src/main/java/commands/Voice.java
+++ GloriousKingJulien/src/main/java/commands/Voice.java
@@ -1,16 +1,23 @@
 package commands;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.lang.ProcessBuilder.Redirect;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -30,7 +37,6 @@ import com.github.kiulian.downloader.model.videos.formats.VideoFormat;
 import com.github.kiulian.downloader.model.videos.formats.VideoWithAudioFormat;
 import bot.Bot;
 import bot.Module;
-import bot.WaveDecoder;
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
@@ -47,11 +53,12 @@ import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
-import net.dv8tion.jda.internal.audio.AudioPacket;
 
 public class Voice implements Module {
 	String topname = "voice";
 	HashMap<String, AudioManager> connections = new HashMap<>();
+	// final static String outputwavpath = "output.wav";
+	final static String outputwavpath = bot.Bot.path + "/videos/output.wav";
 
 	@Override
 	public void run_message(MessageReceivedEvent event) {
@@ -193,96 +200,45 @@ public class Voice implements Module {
 class ASH implements AudioSendHandler {
 	ByteBuffer bb = ByteBuffer.allocate(30720 * 100);
 	byte[] bytes;
-	PipedOutputStream os = (PipedOutputStream) YTDL.os;
-	PipedInputStream pis = new PipedInputStream(30720 * 10);
-	InputStream is = null;
-
-	int pointer = 0;
-	private int framesize;
-	WaveDecoder wd;
-	FileReader fr;
-
-	ASH(char[] obuf) {
-		bytes = new byte[obuf.length];
-		for (int i = 0; i < obuf.length; i++) {
-			bytes[i] = (byte) obuf[i];
-		}
-		AudioPacket ap = new AudioPacket(bytes);
-	}
+	private int framesize = 3840;
+	boolean providing = false;
+	File path = new File("/home/azureuser/bot/videos/output");
+	BufferedInputStream is;
 
 	ASH(File file) throws IOException, UnsupportedAudioFileException {
-		FFmpeg ffmpeg = new FFmpeg(
-				"C:\\Users\\Georg\\Desktop\\anderes\\ffmpeg-N-104630-g203b0e3561-win64-gpl\\ffmpeg-N-104630-g203b0e3561-win64-gpl\\bin\\ffmpeg.exe");
-		FFprobe ffprobe = new FFprobe(
-				"C:\\Users\\Georg\\Desktop\\anderes\\ffmpeg-N-104630-g203b0e3561-win64-gpl\\ffmpeg-N-104630-g203b0e3561-win64-gpl\\bin\\ffprobe.exe");
-		FFmpegBuilder builder = new FFmpegBuilder().setInput(file.getAbsolutePath()).overrideOutputFiles(true)
-				.addOutput("output.wav") // Filename for the destination
-				// .setFormat("wav") // Format is inferred from filename, or can be set
-				.disableSubtitle().disableVideo() // No subtiles
-				.setAudioChannels(2) // Mono audio
-				// .setAudioCodec("libopus") // using the aac codec
-				.setAudioSampleRate(48_000) // at 48KHz
-				.setAudioBitRate(192000) // at 32 kbit/s
-//				.setAudioBitRate(1536000) // at 32 kbit/s
-				.setFormat("s16be").disableSubtitle().disableVideo()
+		String args = "ffmpeg -y -v error -i " + file.getAbsolutePath()
+				+ " -strict experimental -vn -sn -ac 2 -ar 48000 -b 1536000 -f s16be "
+				+ path.getAbsolutePath();
+		ProcessBuilder pb = new ProcessBuilder(args.split(" "))
+				.redirectError(new File("/home/azureuser/bot/videos/error.txt"));
+		Process p = pb.start();
+		System.out.println(p.pid());
+		System.out.println(Arrays.toString(args.split(" ")));
+		long time = System.currentTimeMillis();
+		while (p.isAlive()) {
 
-				.setStrict(FFmpegBuilder.Strict.NORMAL) // Allow FFmpeg to use experimental specs
-				.done();
-
-		FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-
-		// Or run a two-pass encode (which is better quality at the cost of being
-		// slower)
-		executor.createJob(builder).run();
-		System.out.println("job done?");
-		file = new File("output.wav");
-		WaveDecoder wd = new WaveDecoder(file);
-		this.wd = wd;
-		framesize = 3840;
-		//framesize = 30720;
-		fr = wd.fr;
-
-	}
-
-	ASH() {
-		bytes = new byte[30720];
-		for (int i = 0; i < 30720; i += 4) {
-			int sample = (int) (Math.sin((Math.PI * 440) / 48000 * i) * (Math.pow(2, 13)));
-			byte b = (byte) (sample & 0xff);
-			byte a = (byte) ((sample >>> 8) & 0xff);
-			bytes[i] = a;
-			bytes[i + 2] = a;
-			bytes[i + 1] = b;
-			bytes[i + 3] = b;
 		}
-		bb.put(bytes, 0, bytes.length);
-		try {
-			pis.connect(os);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println(System.currentTimeMillis() - time + " for conversa");
+		is = new BufferedInputStream(new FileInputStream(path.getAbsolutePath()),
+				framesize * 50 * 10);
+		providing = true;
 	}
 
 	@Override
 	public boolean canProvide() {
-		return true;
+		return providing;
 	}
 
 	@Override
 	public ByteBuffer provide20MsAudio() {
-		char[] chars = new char[framesize];
-		byte[] tmp = new byte[chars.length];
 		try {
-			fr.read(chars, 0, framesize);
+			bytes = is.readNBytes(framesize);
 		} catch (IOException e) {
+			providing = false;
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for (int i = 0; i < chars.length; i++) {
-			tmp[i] = (byte) chars[i];
-		}
-		return ByteBuffer.wrap(tmp);
+		return ByteBuffer.wrap(bytes);
 	}
 
 	@Override
@@ -297,7 +253,7 @@ class ARH implements AudioReceiveHandler {
 }
 
 class YTDL {
-//	 static OutputStream os = new ByteArrayOutputStream();
+	// static OutputStream os = new ByteArrayOutputStream();
 	static OutputStream os = new PipedOutputStream();
 
 	public static File getVideo(String videoId) { // for url https://www.youtube.com/watch?v=abc12345
@@ -400,5 +356,106 @@ class YTDL {
 		Response<File> vresponse = downloader.downloadVideoFile(vrequest);
 		file = vresponse.data(); // will block current thread
 		return file;
+	}
+}
+
+class WaveDecoder {
+	final static char[] RIFF = { 'R', 'I', 'F', 'F' };
+	final static char[] WAVE = { 'W', 'A', 'V', 'E' };
+	final static char[] fmt_ = { 'f', 'm', 't', ' ' };
+	final static char[] data = { 'd', 'a', 't', 'a' };
+	final static char[] LIST = { 'L', 'I', 'S', 'T' };
+	public final FileReader fr;
+	public File file;
+	public int ChunkSize;
+	public int Subchunk1Size;
+	public int AudioFormat;
+	public int NumChannels;
+	public int SampleRate;
+	public int ByteRate;
+	public int BlockAlign;
+	public int BitsPerSample;
+	public int Subchunk2Size;
+
+	public WaveDecoder(File file) throws IOException {
+		System.out.println(file.length() + " file length");
+		fr = new FileReader(file);
+		char[] header = new char[4];
+		fr.read(header, 0, 4);
+		System.out.println(Arrays.toString(header));
+		for (int i = 0; i < 4; i++) {
+			if (header[i] != WaveDecoder.RIFF[i]) {
+				System.out.println(Arrays.toString(header));
+				// throw new Error("RIFF[" + i + "] was " + header[i]);
+				break;
+			}
+		}
+		fr.read(header, 0, 4);
+		ChunkSize = littleEndianToInt(header);
+		fr.read(header, 0, 4);
+		System.out.println(Arrays.toString(header));
+		for (int i = 0; i < 4; i++) {
+			if (header[i] != WaveDecoder.WAVE[i]) {
+				System.out.println(Arrays.toString(header));
+				// throw new Error("WAVE[" + i + "] was " + header[i]);
+				break;
+			}
+		}
+		fr.read(header, 0, 4);
+		System.out.println(Arrays.toString(header));
+		for (int i = 0; i < 4; i++) {
+			if (header[i] != WaveDecoder.fmt_[i]) {
+				System.out.println(Arrays.toString(header));
+				// throw new Error("fmt [" + i + "] was " + header[i]);.
+				break;
+			}
+		}
+		fr.read(header, 0, 4);
+		Subchunk1Size = littleEndianToInt(header);
+		fr.read(header, 0, 2);
+		AudioFormat = littleEndianToInt(header);
+		fr.read(header, 0, 2);
+		NumChannels = littleEndianToInt(header);
+		fr.read(header, 0, 4);
+		SampleRate = littleEndianToInt(header);
+		fr.read(header, 0, 4);
+		ByteRate = littleEndianToInt(header);
+		fr.read(header, 0, 2);
+		BlockAlign = littleEndianToInt(header);
+		fr.read(header, 0, 2);
+		BitsPerSample = littleEndianToInt(header);
+		fr.read(header, 0, 4);
+		System.out.println(Arrays.toString(header));
+		for (int i = 0; i < 4; i++) {
+			System.out.println(Arrays.toString(header));
+			if (header[i] != WaveDecoder.data[i]) {
+				if (header[i] != WaveDecoder.LIST[i]) {
+					// throw new Error("data[" + i + "] was " + header[i]);
+				}
+				fr.read(header, 0, 4);
+				int ListHeaderSize = littleEndianToInt(header);
+				System.out.println(ListHeaderSize + " removing list chunk");
+				fr.read(new char[ListHeaderSize], 0, ListHeaderSize);
+				break;
+			}
+		}
+		fr.read(header, 0, 4);
+		Subchunk2Size = littleEndianToInt(header);
+
+		System.out.println("ChunkSize:" + ChunkSize + "\nSubchunk1Size:" + Subchunk1Size + "\nAudioFormat:"
+				+ AudioFormat + "\nNumChannels:" + NumChannels + "\nSampleRate:" + SampleRate + "\nByteRate:" + ByteRate
+				+ "\nBlockAlign:" + BlockAlign + "\nBitsPerSample" + BitsPerSample + "\nSubchunk2Size" + Subchunk2Size);
+
+	}
+
+	private int littleEndianToInt(char[] in) {
+		int i3 = in[0];
+		int i2 = in[1];
+		int i1 = in[2];
+		int i0 = in[3];
+		i0 = i0 << 24;
+		i1 = i1 << 16;
+		i2 = i2 << 8;
+		return i0 + i1 + i2 + i3;
 	}
 }
